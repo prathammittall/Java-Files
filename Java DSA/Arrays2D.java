b7ed4024df5702019e2a5ff2f16b0ccbe8c09c2a
prathammittall <prathamv0106@gmail.com>
2025-04-14 01:55:24 +0530

    Diagonal Sum

--- Java DSA/Arrays2D.java
+++ Java DSA/Arrays2D.java
@@ -37,13 +37,43 @@ public class Arrays2D {
 
     }
 
-    
+    public static int diagonalSum(int matrix[][]) {
+        int sum = 0;
+        // --------------Brute Force Method------------
+        // for (int i=0; i<matrix.length; i++) { // Time complexity = O(n²)
+        //     for (int j=0; j<matrix[0].length; j++) {
+        //         if (i == j) {
+        //             sum += matrix[i][j];
+        //         } else if (i+j == matrix.length-1) {
+        //             sum += matrix[i][j];
+        //         }
+        //     }
+        // }
+        // --------------Brute Force Method------------
+        
+        // ----------------More Optimised Method--------------
+        for (int i=0; i<n; i++) { // Time complexity = O(n)
+            // pd
+            sum +=  matrix[i][i];
+            // sd
+            if (i != matrix.length-i-1) {
+                sum += matrix[i][matrix.length-i-1];
+            }
+        }
+        // ----------------More Optimised Method--------------
+
+        return sum;
+    }
+
     public static void main(String[] args) {
         int matrix[][] = {
             {1, 2, 3, 4},
             {5, 6, 7, 8},
-            {9, 10, 11, 12}
+            {9, 10, 11, 12},
+            {13, 14, 15, 16}
         };
-        printSpiral(matrix);
+
+        // printSpiral(matrix);
+        System.out.print("%d", diagonalSum(matrix));
     }
 }
