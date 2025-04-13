b77d79137cf08d5d3bb75aed475f581087a6494b
prathammittall <prathamv0106@gmail.com>
2025-04-13 23:55:24 +0530

    Changes in Arrays2D.java file

--- Java DSA/Arrays2D.java
+++ Java DSA/Arrays2D.java
@@ -1,50 +1,49 @@
 public class Arrays2D {
     public static void printSpiral(int matrix[][]) {
-        int startRow = 0, startCol = 0;
-        int endRow = matrix.length - 1;
-        int endCol = matrix[0].length - 1;
+        int startRow = 0, startCol = 0, EndRow = matrix.length - 1, endCol = matrix[0].length - 1;
 
-        while (startRow <= endRow && startCol <= endCol) {
-            // top
-            for (int i = startCol; i <= endCol; i++) {
-                System.out.print(matrix[startRow][i] + " ");
+        while(startRow <= EndRow && startCol <= endCol) {
+            //top
+            for (int i=startCol; i<=endCol; i++) {
+                System.out.print(matrix[startRow][i]+" ");
             }
 
-            // right
-            for (int i = startRow + 1; i <= endRow; i++) {
-                System.out.print(matrix[i][endCol] + " ");
+            //right
+            for (int i=startRow+1; i<=EndRow; i++) {
+                System.out.print(matrix[i][endCol]+" ");
             }
 
-            // bottom
-            if (startRow < endRow) {
-                for (int i = endCol - 1; i >= startCol; i--) {
-                    System.out.print(matrix[endRow][i] + " ");
+            //bottom
+            for (int i=endCol-1; i>=startCol; i--) {
+                if (startRow == EndRow) {
+                    break;
                 }
+                System.out.print(matrix[EndRow][i]+" ");
             }
-
-            // left
-            if (startCol < endCol) {
-                for (int i = endRow - 1; i > startRow; i--) {
-                    System.out.print(matrix[i][startCol] + " ");
+            //left
+            for (int i=EndRow-1; i>=startRow+1; i--) {
+                if (startCol == endCol) {
+                    break;
                 }
+                System.out.print(matrix[i][startCol]+" ");
             }
 
             startRow++;
-            endRow--;
             startCol++;
+            EndRow--;
             endCol--;
         }
         System.out.println();
+
     }
 
+    
     public static void main(String[] args) {
         int matrix[][] = {
             {1, 2, 3, 4},
             {5, 6, 7, 8},
-            {9, 10, 11, 12},
-            {13, 14, 15, 16}
+            {9, 10, 11, 12}
         };
-
         printSpiral(matrix);
     }
 }
